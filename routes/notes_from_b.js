d5ab65edf3c136af2f954f863dcf2d455274adce
KZK1905 <kurotamago1905@outlook.jp>
2025-01-17 10:42:12 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const cors = require('cors'); //corsミドルウェアを追加
+const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
-
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
@@ -17,10 +16,11 @@ router.get('/', async (req, res) => {
   const database = client.db('notes');
   const notes = database.collection('notes');
 
-  // idが１のドキュメントを取得
-  const query = { id: 2 };
-  const note = await notes.findOne(query);
+
+  // 全てのドキュメントを取得
+  const note = await notes.find({}).toArray();
 
   res.json(note);
 })
+
 module.exports = router;
